d30720ac8518b91ed20b3efecd194aba095fab16
Yike Gao <yikegao8@gmail.com>
2022-10-21 11:08:52 +0100

    10.21

--- card/src/App.js
+++ card/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import ImageSlider from './components/ImageSlider';
 function App() {
   const slides = [
-    {content: "AIB allows you manage your bank accounr on-the-go, track your expenses and set aside money in real-time. Open yours in minutes right from your smartphone", title :"Join us at AIB"},
+    {content: "AIB allows you manage your bank accounr on-the-go, track your expenses and set aside money in real-time. Open yours in minutes right from your smart phone.", title :"Join us at AIB"},
     {content: "I'm second", title :"Second"},
     {content: "I'm the third", title :"Third"},
   ]
